2105a67a73499f8612a88c182d41c3fffb8a13e5
pohlan <annegret.pohle@outlook.com>
2024-02-20 16:16:44 -0900

    added McNabb positions (but commented out) and refined plotting and saving

--- terminus_change/terminus_area_change.py
+++ terminus_change/terminus_area_change.py
@@ -7,20 +7,23 @@ from shapely import Point
 import datetime as dt
 import statsmodels.api as sm
 
-local_path = "/home/annegret/myfolder/Hubbard2024/terminus_change/"
+data_path = "/home/annegret/Projects/Hubbard2024/data/terminus_data/"     # folder where all the data for terminus position and terminus sections is stored
+fig_path  = "/home/annegret/Projects/Hubbard2024/terminus_change/figures/"           # folder to save figures
 
 # read in terminus traces (UTM07)
-terminus_path = local_path + "terminus_data/Hubbard_2017_2021_UTM07.shp"
+terminus_path = data_path + "Hubbard_2017_2021_UTM07.gpkg"
 df_terminus = gpd.read_file(terminus_path)   # df_terminus.geometry is already in LineString format
 # Mcnabb terminus data
-terminus_mcnabb_path = local_path + "terminus_data/hubbard.shp"
+terminus_mcnabb_path = data_path + "Hubbard_McNabb.gpkg"
 df_terminus_mcnabb = gpd.read_file(terminus_mcnabb_path)
 
 # read in section polygons
-path_sections = local_path + "terminus_data/terminus_sections_rectangle.gpkg"
+path_sections = data_path + "terminus_sections_rectangle.gpkg"
 df_sec = gpd.read_file(path_sections) # df_sA.geometry is already in Polygon format
 df_sec = df_sec.to_crs(df_terminus.crs)
+df_sec.sort_values(by=["section"], inplace=True)
 
+# define some functions for deriving the area change
 def area_rule(name, geom1, geom2):
     x1, y1 = geom1.exterior.coords.xy
     x2, y2 = geom2.exterior.coords.xy
@@ -47,58 +50,57 @@ def get_width(geom):
     width = min(edge_length)
     return width
 
-# split polygon with terminus line
-list_areas = []
-n = 0
-for (day, terminus_line) in zip(df_terminus.Date, df_terminus.geometry):
-    newrow = {}
-    newrow["Date"] = dt.datetime.strptime(day, "%Y-%m-%d").date()
+def append_positions_by_section(newrow, terminus_line):
     for (sect_name, sect_geom) in zip(df_sec.section, df_sec.geometry):
         split_pols = split(sect_geom, terminus_line)
         if len(list(split_pols.geoms)) != 2:
             continue
-        # assert list(split_pols.geoms)[0].area > list(split_pols.geoms)[1].area
         glacier_pol = area_rule(sect_name, split_pols.geoms[0], split_pols.geoms[1])
         width       = get_width(sect_geom)
         newrow[sect_name] = glacier_pol.area / width
+
+# split polygon with terminus line
+list_areas = []
+# Cryohackathon terminus lines
+for (day, terminus_line) in zip(df_terminus.Date, df_terminus.geometry):
+    newrow = {}
+    newrow["Date"] = dt.datetime.strptime(day, "%Y-%m-%d").date()
+    append_positions_by_section(newrow, terminus_line)
     list_areas.append(newrow)
-    n += 1
+
+# add McNabb terminus lines (for long term trend, don't help much for seasonal variability)
+# for (yr, day, terminus_line) in zip(df_terminus_mcnabb.year, df_terminus_mcnabb.doy, df_terminus_mcnabb.geometry):
+#     newrow = {}
+#     day = str(int(day)+1)              # to handle days = 0 which exist in this dataset
+#     newrow["Date"] = dt.datetime.strptime(yr + "-" + day, "%Y-%j").date()
+#     append_positions_by_section(newrow, terminus_line)
+#     list_areas.append(newrow)
+
 df_areas = pd.DataFrame(list_areas)
 df_areas = df_areas.sort_values(by=["Date"])
 
-# plt.plot(df_areas.Date,df_areas.section_A / np.max(df_areas.section_A), label="section A")
-# plt.plot(df_areas.Date,df_areas.sectionB_2 / np.max(df_areas.sectionB_2), label="section B2")
-# plt.plot(df_areas.Date,df_areas.sectionD / np.max(df_areas.sectionD), label="section D")
-# plt.legend()
-# plt.savefig("terminus_vs_time.jpg")
-
-# use lowess filter to smooth time series
+# prepare lowess filter to smooth time series, easier on day index rather than datetime format
 lowess = sm.nonparametric.lowess
 t0 = dt.datetime.strptime("2017-01-01", "%Y-%m-%d").date()
-tend = dt.datetime.strptime("2021-12-31", "%Y-%m-%d").date()
-
 t_index = np.zeros(len(df_areas.Date))
 for (i, da) in enumerate(df_areas.Date):
     deltat = da - t0
     t_index[i] = deltat.days
-t_index
 
-fig, ax = plt.subplots(2, 1, sharey=True)
-for s in df_sec.section:
-    z = lowess(df_areas[s] - np.mean(df_areas[s]), t_index, frac=1/20)
-    t_smooth = np.zeros(z.shape[0], dtype=dt.date)
-    for (i,zi) in enumerate(z[:,0]):
-        t_smooth[i] = t0 + dt.timedelta(days=zi)
-    # plt.figure()
-    # plt.plot(df_areas.Date,df_areas[s] - np.mean(df_areas[s]), label=s, lw=1, alpha=0.9)
-    if s == "section_A" or s == "section_B1":
-        ax[0].plot(t_smooth, z[:,1], label=s)
-        ax[0].legend()
-    elif  s == "section_B2" or s == "section_C":
-        ax[1].plot(t_smooth, z[:,1], label=s)
-        ax[1].legend()
+# smooth and plot
+df_terminus_smooth = pd.DataFrame({"Date":df_areas.Date})
+plt.figure(figsize=(15,10))
+plt.rcParams['font.size'] = 14
+cols = ["brown", "pink", "darkgreen", "lightgreen", "grey"]    # only give four colours to only plot the first four sections
+for (s,c) in zip(df_sec.section,cols):
+    z = lowess(df_areas[s] - np.mean(df_areas[s]), t_index, frac=1/20, xvals=t_index)
+    df_terminus_smooth[s+" [m]"] = z
+    plt.plot(df_areas.Date, z, label=s, color=c)
+    plt.legend()
+plt.ylim(-330,230)
+plt.ylabel("rel. terminus pos. [m]")
+plt.xlabel("Date")
+plt.savefig(fig_path + "smoothed_termini.jpg")
 
-ax[0].set_ylabel("rel. terminus pos. [m]")
-ax[1].set_ylabel("rel. terminus pos. [m]")
-ax[1].set_xlabel("Date")
-plt.savefig(local_path + "smoothed_termini.jpg")
+# save smoothed time series
+df_terminus_smooth.to_csv(data_path+"terminus_position_smooth.csv", index=False)
