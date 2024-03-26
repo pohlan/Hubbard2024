929bdeaa95d49ff6aafdf9efb74868b3d4bff8bf
amyjenson <ajjenson@alaska.edu>
2024-03-25 17:34:01 -0800

    added to mesh generation files

--- modeling/elmer/mesh/s1_make_geo_and_mesh.sh
+++ modeling/elmer/mesh/s1_make_geo_and_mesh.sh
@@ -6,6 +6,6 @@ fi
 
 #python Contour2geo.py -r 500 -i outline/hubbard_contour.shp -o hubbard.geo # convert shape file into geo file
 
-python Contour2geo.py -r 150.0 -i outline/hubbard_contour2.csv -o hubbard_mesh.geo
+python Contour2geo.py -r 150.0 -i ./outline/hubbard_contour.dat -o hubbard_mesh.geo
 
 gmsh hubbard.geo -2 
