93b791129d6638f3021a2fe8129cec82053839dc
amyjenson <ajjenson@alaska.edu>
2024-04-03 16:57:01 -0800

    update glads and just ice mesh
    replaced nans in millan velocity with 0.00

--- modeling/elmer/mesh/s1_make_geo_and_mesh.sh
+++ modeling/elmer/mesh/s1_make_geo_and_mesh.sh
@@ -4,8 +4,8 @@ if test -f "hubbard.geo"; then
     rm hubbard.geo
 fi 
 
-#python Contour2geo.py -r 500 -i outline/hubbard_contour.shp -o hubbard.geo # convert shape file into geo file
+#python Contour2geo.py -r 500 -i outline/hubbard_ice_contour.shp -o hubbard_ice.geo # convert shape file into geo file
 
-python Contour2geo.py -r 150.0 -i ./outline/hubbard_contour.dat -o hubbard_mesh.geo
+python Contour2geo.py -r 150.0 -i ./outline/hubbard_ice_contour.dat -o hubbard_ice_mesh.geo
 
-gmsh hubbard_mesh.geo -2 
+gmsh hubbard_ice_mesh.geo -2 
