0d8ca5626b89427126af6075e178aa8bddb6e03f
amyjenson <ajjenson@alaska.edu>
2024-05-23 11:33:37 -0800

    add force balance

--- make_paper_figures/strength_along_centerline/s1_reproj_DEM.sh
+++ make_paper_figures/strength_along_centerline/s1_reproj_DEM.sh
@@ -6,3 +6,11 @@ if test -f "ifsar_hubbardDEM_reproj.tif"; then
 fi
 
 gdalwarp -t_srs EPSG:3413 -tr 5 5 ifsar_hubbardDEM.tif ifsar_hubbardDEM_reproj.tif
+
+
+
+if test -f "hubbard_bedrock_icebridge_reproj.tif"; then
+    rm hubbard_bedrock_icebridge_reproj.tif
+fi
+
+gdalwarp -t_srs EPSG:3413 hubbard_bedrock_icebridge.tif hubbard_bedrock_icebridge_reproj.tif
