b88c190666c537aefda9d84ed0e7d786723a6f16
amyjenson <ajjenson@alaska.edu>
2026-01-30 15:47:55 -0900

    update speed map script and name schematic fig

--- make_paper_figures/basemap/basemap.py
+++ make_paper_figures/basemap/basemap.py
@@ -10,7 +10,6 @@ from pyproj import Proj, transform
 import geopandas as gpd
 import matplotlib.cm as cm
 import matplotlib.colors as mcolors
-#from matplotlib_scalebar.scalebar import ScaleBar
 
 fs = 24  # font size
 
@@ -56,42 +55,19 @@ with rio.open("RGB_Temp.tif") as src2:
 
 ########################
 
-# dense points around ice fall
-sites = np.arange(1,21,1)
+sites = np.arange(1, 21, 1)
 
 # Use a colormap to assign unique colors
-cmap = cm.get_cmap('tab20', len(sites))
+cmap = cm.get_cmap("tab20", len(sites))
 
-for idx, site in enumerate(sites): 
-    points = pd.read_csv(f"./get_flowline/flowline_{site}.csv")
+for idx, site in enumerate(sites):
+    points = pd.read_csv(f"flowline_{site}.csv")
     transformer = Transformer.from_crs("epsg:3413", "epsg:32607")  # UTM 6N
     points_X, points_Y = transformer.transform(points.x.to_numpy(), points.y.to_numpy())
     color = cmap(idx)
     ax.plot(points_X, points_Y, label=f"Flowline {site}", color=color)
 
 ax.legend()
-
-########################
-# add inset map
-
-# Read the CSV file into a DataFrame
-# df = gpd.read_file("Alaska_outline_cropped.shp")
-# df = df.to_crs(epsg=32607)
-
-# # Add a subplot with a black background
-# ax_map = fig.add_axes([0.15, 0.675, 0.2, 0.2], facecolor="white")
-
-# # Plot the shapefile on the map subplot
-# df.plot(ax=ax_map, color="black")
-# ax_map.scatter([590839], [6657969], alpha=1, c="red", s=100)
-# # ax_map.axis('off')
-# # Turn off the ticks and axis labels
-# ax_map.tick_params(
-#     axis="both", which="both", bottom=False, top=False, left=False, right=False
-# )
-# ax_map.set_xticks([])
-# ax_map.set_yticks([])
-
 
 # Get all text objects in the figure
 text_objs = plt.gcf().findobj(plt.Text)
